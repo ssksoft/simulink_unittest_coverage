61c620141bb40276d9db9e2befb8b64e070864a7
ssk <sasat.s.93@gmail.com>
2020-05-31 16:35:41 +0900

    gain_testの実行カバレッジ取得成功

--- model/test/html_parse_pandas_bs.py
+++ model/test/html_parse_pandas_bs.py
@@ -1,6 +1,13 @@
 import pandas
-url = 'file:///C:/Users/sasat/Desktop/simulink_unittest_coverage/slcov_output/CoverageDetails/docked__cov__report__gain__Normal.html'
-fetched_dataframes = pandas.io.html.read_html(url, header=0, index_col=0)
+gain_url = 'file:///C:/Users/sasat/Desktop/simulink_unittest_coverage/model/test/slcov_output/CoverageDetails/docked__cov__report__gain__Normal.html'
+
+switch_url = 'file:///C:/Users/sasat/Desktop/simulink_unittest_coverage/model/test/slcov_output/CoverageDetails/docked__cov__report__switch_func__Normal.html'
+
+truthtable_url = 'file:///C:/Users/sasat/Desktop/simulink_unittest_coverage/model/test/slcov_output/CoverageDetails/docked__cov__report__truthtable_func__Normal.html'
+
+urls = [gain_url, switch_url, truthtable_url]
+
+fetched_dataframes = pandas.io.html.read_html(urls[0], header=0, index_col=0)
 i = 0
 for fetched_dataframe in fetched_dataframes:
     print('table{}'.format(i))
@@ -8,4 +15,4 @@ for fetched_dataframe in fetched_dataframes:
     print('##########################')
     i = i+1
 
-print(fetched_dataframes[12].loc['実行', 'カバレッジ'])
+print(fetched_dataframes[11].loc['実行', 'カバレッジ'])
