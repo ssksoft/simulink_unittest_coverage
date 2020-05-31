b6b1c88412a5abc56e359a75eb7c33d131ed2550
ssk <sasat.s.93@gmail.com>
2020-05-31 17:33:41 +0900

    3モデルのカバレッジ計測まとめ成功
    要リファクタリング

--- model/test/html_parse_pandas_bs.py
+++ model/test/html_parse_pandas_bs.py
@@ -7,48 +7,36 @@ truthtable_url = 'file:///C:/Users/sasat/Desktop/simulink_unittest_coverage/mode
 
 urls = [gain_url, switch_url, truthtable_url]
 
-# coverage_overall_df = pd.DataFrame({'モデル名': 'init',
-#                                     '実行カバレッジ': '0',
-#                                     '判定カバレッジ': '0',
-#                                     '条件カバレッジ': '0'
-#                                     })
-
-model_name = ['1. gain', '1. switch_func', '1. truthtable_func']
+model_names = ['1. gain', '1. switch_func', '1. truthtable_func']
 
 coverage_overall_df = pd.read_csv('setting/coverage_overall_template.csv')
 
-fetched_dataframes = pd.io.html.read_html(urls[0], header=1, index_col=0)
-i = 0
-for fetched_dataframe in fetched_dataframes:
-    print('table{}'.format(i))
-    print(fetched_dataframe)
-    print('##########################')
-    i = i+1
+for current_model, model_name in zip(range(len(model_names)), model_names):
+    fetched_dataframes = pd.io.html.read_html(
+        urls[current_model], header=1, index_col=0)
 
-coverage_abstract = fetched_dataframes[4]
-# print(coverage_abstract)
-print(coverage_abstract.loc[model_name[0], '実行'])
+    coverage_abstract = fetched_dataframes[4]
+    print(coverage_abstract)
 
-# coverage_abstract.to_csv('trial.csv', encoding="utf-8_sig")
+    print(model_name)
+    print('のカバレッジを集計中')
+    coverage_overall_df.loc[current_model, 'モデル名'] = model_name
+    try:
+        coverage_overall_df.loc[current_model,
+                                '実行'] = coverage_abstract.loc[model_name, '実行']
+    except:
+        coverage_overall_df.loc[current_model, '実行'] = '-'
+    try:
+        coverage_overall_df.loc[current_model,
+                                '判定'] = coverage_abstract.loc[model_name, '判定']
+    except:
+        coverage_overall_df.loc[current_model, '判定'] = '-'
 
-# coverage_overall_df.loc[0, 'モデル名'] = 'gain'
-# coverage_overall_df.loc[0, '実行'] \
-#     = coverage_abstract.loc['実行', 'カバレッジ']
-# try:
-#     coverage_overall_df.loc[0, '判定'] \
-#         = coverage_abstract.loc['判定', 'カバレッジ']
-# except:
-#     coverage_overall_df.loc[0, '判定'] \
-#         = '-'
+    try:
+        coverage_overall_df.loc[current_model,
+                                '条件'] = coverage_abstract.loc[model_name, '条件']
+    except:
+        coverage_overall_df.loc[current_model, '条件'] = '-'
 
-# try:
-#     coverage_overall_df.loc[0, '条件'] \
-#         = coverage_abstract.loc['条件', 'カバレッジ']
-# except:
-#     coverage_overall_df.loc[0, '条件'] \
-#         = '-'
-
-# coverage_overall_df.to_csv('coverage_overall.csv',
-#                            encoding="utf-8_sig", index=None)
-
-# # print(coverage_overall_df)
+coverage_overall_df.to_csv('coverage_overall.csv',
+                           encoding="utf-8_sig",   index=None)
